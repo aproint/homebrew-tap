3f1f83323de11339de3bf8d190fc9334aedea4b1
byF <592682+byF@users.noreply.github.com>
2026-07-09 09:01:44 +0000

    chore: upgrade copilot-cli to v0.0.0-rc.1

--- Formula/copilot-cli.rb
+++ Formula/copilot-cli.rb
@@ -10,13 +10,13 @@ class CopilotCli < Formula
   on_macos do
     on_arm do
       url "https://github.com/aproint/copilot-cli/releases/download/v0.0.0-rc.1/copilot-darwin-arm64",
-          using: NoUnzipCurlDownloadStrategy
+using: NoUnzipCurlDownloadStrategy
       sha256 "0267e98ab6d31f7205d6907e3d9617724e77ccb51169bf287db1bdd0a677c978"
     end
 
     on_intel do
       url "https://github.com/aproint/copilot-cli/releases/download/v0.0.0-rc.1/copilot-darwin-amd64",
-          using: NoUnzipCurlDownloadStrategy
+using: NoUnzipCurlDownloadStrategy
       sha256 "b4d510c81ccc3026ff4269663f6db0e63d4124145f400bfc73f5426e281720ba"
     end
   end
@@ -26,6 +26,6 @@ class CopilotCli < Formula
   end
 
   test do
-    system "#{bin}/copilot", "--help"
+    system "\#{bin}/copilot", "--help"
   end
 end
